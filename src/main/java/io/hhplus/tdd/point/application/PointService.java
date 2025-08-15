43a6400dc15f0eb3f2d8f9c6113d73d0b4bc7ba9
zun <k5orkrj@gmail.com>
2025-08-15 22:54:47 +0900

    feat: add method to get user point histories (#1)

--- src/main/java/io/hhplus/tdd/point/application/PointService.java
+++ src/main/java/io/hhplus/tdd/point/application/PointService.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd.point.application;
 
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.domain.PointHistory;
 import io.hhplus.tdd.point.domain.UserPoint;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,9 @@ public class PointService {
 
   public UserPoint getUserPoints(long userId) {
     return this.userPoint.selectById(userId);
+  }
+
+  public List<PointHistory> getUserPointHistories(long userId) {
+    return this.pointHistory.selectAllByUserId(userId);
   }
 }
