13aba0ff60d74bac5744278320a43c219d1edfdd
zun <k5orkrj@gmail.com>
2025-08-16 00:18:49 +0900

    feat: add methods to charge user points (#1)

--- src/main/java/io/hhplus/tdd/point/application/PointService.java
+++ src/main/java/io/hhplus/tdd/point/application/PointService.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point.application;
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import io.hhplus.tdd.point.domain.PointHistory;
+import io.hhplus.tdd.point.domain.TransactionType;
 import io.hhplus.tdd.point.domain.UserPoint;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,5 +22,28 @@ public class PointService {
 
   public List<PointHistory> getUserPointHistories(long userId) {
     return this.pointHistory.selectAllByUserId(userId);
+  }
+
+  public UserPoint chargePoints(long userId, long amount) {
+    long checkedAmount = this.checkChargeAmount(amount);
+
+    try{
+      long currentPoints = this.userPoint.selectById(userId).point();
+      UserPoint chargedUserPoints = this.userPoint.insertOrUpdate(userId, currentPoints + checkedAmount);
+      this.pointHistory.insert(userId, checkedAmount, TransactionType.CHARGE, System.currentTimeMillis());
+      return chargedUserPoints;
+
+    } catch (Exception e){
+      throw new RuntimeException("포인트 충전 중 오류가 발생했습니다.", e);
+    }
+  }
+
+  // -------------------
+  private long checkChargeAmount(long amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("충전 금액은 0원보다 커야 합니다.");
+    }
+
+    return amount;
   }
 }
