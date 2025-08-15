09504ddb7d69a9fd4ab3be20129c2abe12ab48d3
zun <k5orkrj@gmail.com>
2025-08-15 22:55:36 +0900

    test: add unit tests for getUserPointHistories() (#1)

--- src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/application/PointServiceTest.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd.point.application;
 
 import static org.assertj.core.api.Assertions.*;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.domain.TransactionType;
 import io.hhplus.tdd.point.domain.UserPoint;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,13 +14,15 @@ import org.junit.jupiter.api.Test;
 
 class PointServiceTest {
 
-  PointService sut;
   UserPointTable userPointTable;
+  PointHistoryTable pointHistoryTable;
+  PointService sut;
 
   @BeforeEach
   void setUp() {
     userPointTable = new UserPointTable();
-    sut = new PointService(userPointTable, null);
+    pointHistoryTable = new PointHistoryTable();
+    sut = new PointService(userPointTable, pointHistoryTable);
   }
 
   @AfterEach
@@ -58,6 +62,56 @@ class PointServiceTest {
       // then
       assertThat(userPoint).isNotNull();
       assertThat(userPoint.point()).isEqualTo(100L);
+    }
+  }
+
+  @Nested
+  @DisplayName("특정 유저의 포인트 충전/이용 내역을 조회하는 기능")
+  class getUserPointHistories {
+
+    @Test
+    @DisplayName("유저 포인트 충전/이용 내역이 없는 경우 빈 리스트를 반환한다.")
+    void givenUserId_whenGetUserPointHistories_thenReturnEmptyList() {
+      // given
+      long userId = 1L;
+
+      // when
+      var histories = sut.getUserPointHistories(userId);
+
+      // then
+      assertThat(histories).isEmpty();
+    }
+
+    @Test
+    @DisplayName("유저 포인트 충전 내역이 있는 경우 해당 내역을 반환한다.")
+    void givenUserId_whenGetUserPointHistories_thenReturnChargeHistories() {
+      // given
+      long userId = 1L;
+      pointHistoryTable.insert(userId, 1000L, TransactionType.CHARGE, System.currentTimeMillis());
+
+      // when
+      var histories = sut.getUserPointHistories(userId);
+
+      // then
+      assertThat(histories).hasSize(1);
+      assertThat(histories.get(0).amount()).isEqualTo(1000L);
+    }
+
+    @Test
+    @DisplayName("유저 포인트 이용 내역이 있는 경우 해당 내역을 반환한다.")
+    void givenUserId_whenGetUserPointHistories_thenReturnUseHistories() {
+      // given
+      long userId = 1L;
+      pointHistoryTable.insert(userId, 1000L, TransactionType.CHARGE, System.currentTimeMillis());
+      pointHistoryTable.insert(userId, -500L, TransactionType.USE, System.currentTimeMillis());
+
+      // when
+      var histories = sut.getUserPointHistories(userId);
+
+      // then
+      assertThat(histories).hasSize(2);
+      assertThat(histories.get(0).amount()).isEqualTo(1000L);
+      assertThat(histories.get(1).amount()).isEqualTo(-500L);
     }
   }
 }
