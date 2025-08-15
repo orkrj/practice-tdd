55467e6853bf78f4f62e22cc4f6c732c93b7adfb
zun <k5orkrj@gmail.com>
2025-08-15 22:31:23 +0900

    feat: connect point endpoint to getUserPoints in PointService (#1)

--- src/main/java/io/hhplus/tdd/point/presentation/PointController.java
+++ src/main/java/io/hhplus/tdd/point/presentation/PointController.java
@@ -1,23 +1,28 @@
 package io.hhplus.tdd.point.presentation;
 
+import io.hhplus.tdd.point.application.PointService;
 import io.hhplus.tdd.point.domain.PointHistory;
 import io.hhplus.tdd.point.domain.UserPoint;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
   private static final Logger log = LoggerFactory.getLogger(PointController.class);
-
+  
+  private final PointService pointService; 
+  
   /** TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요. */
   @GetMapping("{id}")
-  public UserPoint point(@PathVariable long id) {
-    return new UserPoint(0, 0, 0);
+  public ResponseEntity<UserPoint> point(@PathVariable long id) {
+    return ResponseEntity.ok(this.pointService.getUserPoints(id));
   }
 
   /** TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요. */
