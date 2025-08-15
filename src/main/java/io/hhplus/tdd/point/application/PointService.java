0f8e85d5ebe48afebeea4e43e0a94143128715d1
zun <k5orkrj@gmail.com>
2025-08-15 22:21:16 +0900

    feat: add method to get user points (#1)

---
+++ src/main/java/io/hhplus/tdd/point/application/PointService.java
@@ -0,0 +1,19 @@
+package io.hhplus.tdd.point.application;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.domain.UserPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+  private final UserPointTable userPoint;
+  private final PointHistoryTable pointHistory;
+
+  public UserPoint getUserPoint(long userId) {
+    return this.userPoint.selectById(userId);
+  }
+}
